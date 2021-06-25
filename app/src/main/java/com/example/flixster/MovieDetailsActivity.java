c5d48621c9b9c806f4669d92ad704ea7e350c891
pau-cardoso <A01701490@itesm.mx>
2021-06-25 11:58:27 -0500

    Add title, ratingBar, and overview to layout

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -4,20 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.flixster.models.Movie;
 
 import org.parceler.Parcels;
+import org.w3c.dom.Text;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
     Movie movie;
+    ImageView ivPoster;
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvOverview = (TextView) findViewById(R.id.tvOverview);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivPoster = (ImageView) findViewById(R.id.ivPoster);
+
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
+        // set the image, title and overview
+        //ivPoster.setImageDrawable();
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+
+        // vote average is 0..10, convert to 0..5 by dividing by 2
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage / 2.0f);
     }
 }
